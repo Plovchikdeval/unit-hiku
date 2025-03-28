5bea20c8e8a0bf7cd83c07afb6dc7edc3ad7ab8c
Dev3Quest <plov99502@gmail.com>
2025-03-28 22:17:23 +0500

    Update unit-hiku.py

--- unit-hiku.py
+++ unit-hiku.py
@@ -1,5 +1,5 @@
-__version__ = (2, 0, 1)
-# change-log: fix more bugs and fix info module
+__version__ = (2, 0, 2)
+# change-log: Change name to update cmd
 
 """
 888    d8P   .d8888b.  888    888     888b     d888  .d88888b.  8888888b.   .d8888b.  
