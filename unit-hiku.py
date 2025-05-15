b2f4da940866ded29051beeeb15e7077a7b3677e
Dev3Quest <plov99502@gmail.com>
2025-05-15 13:04:26 +0500

    Update unit-hiku.py

--- unit-hiku.py
+++ unit-hiku.py
@@ -1,5 +1,5 @@
 __version__ = (2, 6, 7)
-# change-log: check my fix error
+# change-log: fix bugs, force update
 
 """
 888    d8P   .d8888b.  888    888     888b     d888  .d88888b.  8888888b.   .d8888b.  
